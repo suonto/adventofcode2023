37560e7b33b7dd7e2361befc7f1ea0c0dd21e850
Markus Suonto <markus.suonto@withsecure.com>
2023-12-07 11:33:26 +0200

    feat(day1): part 2

--- day1/index.ts
+++ day1/index.ts
@@ -1,33 +1,36 @@
-import { open } from 'node:fs/promises';
-import path from 'node:path';
+import { open } from "node:fs/promises";
+import path from "node:path";
 
 const map = {
-  one: '1',
-  two: '2',
-  three: '3',
-  four: '4',
-  five: '5',
-  six: '6',
-  seven: '7',
-  eight: '8',
-  nine: '9'
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
 };
 
 (async () => {
-    const file = await open(path.join(__dirname, '.', 'input.txt'));
-  
-    let sum = 0;
-    for await (const line of file.readLines()) {
-      console.log(line);
-      const pattern = new RegExp(`\\d{1}|(${Object.keys(map).join('|')})`, 'g');
-      const match = line.match(pattern);
-      console.log('match', match)
-      const first = map[match.at(0)] ?? match.at(0);
-      const last = map[match.at(-1)] ?? match.at(-1);
+  const file = await open(path.join(__dirname, ".", "input.txt"));
 
-      const result = `${first}${last}`;
-      sum += Number.parseInt(result);
-      console.log(result, sum)
+  let sum = 0;
+  for await (const line of file.readLines()) {
+    console.log(line);
+    const matches = line.matchAll(
+      new RegExp(`(?=(\\d{1}|(${Object.keys(map).join("|")})))`, "g")
+    );
 
-    }
-  })();
+    const arr: string[] = Array.from(matches).map((match) => match[1]);
+
+    console.log(arr);
+    const first = map[arr.at(0)] ?? arr.at(0);
+    const last = map[arr.at(-1)] ?? arr.at(-1);
+
+    const result = `${first}${last}`;
+    sum += Number.parseInt(result);
+    console.log(result, sum);
+  }
+})();
