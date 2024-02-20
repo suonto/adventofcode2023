20c4e456854f462005fdd8a62fa236f9227b48dd
Markus Suonto <markus.suonto@gmail.com>
2024-02-20 18:34:24 +0000

    no dice

--- day25/index.ts
+++ day25/index.ts
@@ -93,11 +93,135 @@ class Connection {
       path: [...other.path],
     });
   }
+
+  toString() {
+    return `${this.src.name} -> ${this.dest.name}: [${this.path.map(
+      (h) => h.name,
+    )}]`;
+  }
 }
+
+const advance = (conns: Connection[]): void => {
+  const newConns: Connection[] = [];
+  for (let conn = conns.shift(); conn; conn = conns.shift()) {
+    for (const newConn of conn.advance()) {
+      newConns.push(newConn);
+    }
+  }
+  for (const newConn of newConns) {
+    if (
+      newConns.every((c) => !c.path.slice(0, -1).includes(newConn.path.at(-1)!))
+    ) {
+      conns.push(newConn);
+    }
+  }
+};
+
+const match = (params: {
+  conns: Connection[];
+  counterparts: Connection[];
+}): Connection[] => {
+  const dMatch = debug('match');
+  const { conns, counterparts } = params;
+  const finished: Connection[] = [];
+  const inProgress: Connection[] = [];
+  for (let conn = conns.shift(); conn; conn = conns.shift()) {
+    if (conn.finished()) {
+      finished.push(conn);
+    } else {
+      let connFinished = false;
+      const remainingCounterparts: Connection[] = [];
+      for (
+        let counterpart = counterparts.shift();
+        counterpart;
+        counterpart = counterparts.shift()
+      ) {
+        if (conn.path.at(-1) && conn.path.at(-1) === counterpart.path.at(-1)) {
+          conn.path.push(
+            ...counterpart.path.slice(0, -1).reverse(),
+            counterpart.src,
+          );
+          connFinished = true;
+          finished.push(conn);
+        } else if (!counterpart.finished()) {
+          remainingCounterparts.push(counterpart);
+        }
+      }
+      if (!connFinished) {
+        inProgress.push(conn);
+      }
+      counterparts.push(...remainingCounterparts);
+    }
+  }
+  conns.push(...inProgress);
+  dMatch(finished.map((c) => c.toString()));
+  return finished;
+};
 
 class Network {
   private readonly d = debug('network');
   hubs: Hub[] = [];
+
+  conns(a: Hub, b: Hub): Connection[] {
+    const dConns = debug('conns');
+    const finished: Connection[] = [];
+    const conns: Connection[] = [
+      new Connection({
+        src: a,
+        dest: b,
+        path: [],
+      }),
+    ];
+    const counterparts: Connection[] = [
+      new Connection({
+        src: b,
+        dest: a,
+        path: [],
+      }),
+    ];
+    let round = 0;
+    while (conns.length && counterparts.length) {
+      dConns('advance conns');
+      advance(conns);
+      dConns(
+        'conns',
+        conns.map((c) => c.toString()),
+      );
+      dConns(
+        'counterparts',
+        counterparts.map((c) => c.toString()),
+      );
+      finished.push(...match({ conns, counterparts }));
+      dConns('advance counterparts');
+      advance(counterparts);
+      dConns(
+        'conns',
+        conns.map((c) => c.toString()),
+      );
+      dConns(
+        'counterparts',
+        counterparts.map((c) => c.toString()),
+      );
+      finished.push(...match({ conns, counterparts }));
+
+      round++;
+    }
+
+    const final: Connection[] = [];
+    for (const conn of finished) {
+      if (final.every((c) => !c.intercects(conn))) {
+        final.push(conn);
+      }
+    }
+    this.d(
+      'conns',
+      a.name,
+      '->',
+      b.name,
+      final.map((c) => c.path.map((h) => h.name)),
+    );
+    return final;
+  }
 
   uniqueConns(a: Hub, b: Hub): Connection[] {
     const dUniqueConns = debug('uniqueConns');
@@ -153,7 +277,8 @@ const main = async () => {
 
   const groupB: Hub[] = [];
   for (const candidate of network.hubs.filter((h) => h.name !== rootA)) {
-    if (network.uniqueConns(groupA[0], candidate).length > 3) {
+    // for (const candidate of network.hubs.filter((h) => h.name === 'rhn')) {
+    if (network.conns(groupA[0], candidate).length > 3) {
       dMain('same group', rootA, candidate.name);
       groupA.push(candidate);
     } else {
