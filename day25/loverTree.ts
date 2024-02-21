d5d0f83644009df2f52a5c913806837a4e041200
Markus Suonto <markus.suonto@gmail.com>
2024-02-21 11:09:25 +0000

    feat(day25): LoverTree initial commit

---
+++ day25/loverTree.ts
@@ -0,0 +1,18 @@
+/**
+ * A LoverTree is a magical tree that grows in pairs.
+ * It's the garden elfs favourite tree species.
+ *
+ * The pair grows in turns. First one grows, then the other.
+ * A branch never grows to a space where another branch can also grow.
+ * Who knows, if that's a space where the lover grows in the future?
+ *
+ * The purpose of a lover tree pair is to first grow safely without
+ * blocking self or the other.
+ *
+ * After growing, the trees extend individual branches to meet in a free
+ * space between them. The more branches meet, the better the embracement.
+ *
+ * By understanding these trees, we could perhaps understand the wires.
+ * Nature is so amazing. Always has a solution to everything.
+ */
+export class LoverTree {}
